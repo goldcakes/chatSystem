f3c7b84f758f6e23001508613d4c1ba27d29a24f
Kyle <kyle.anders12@outlook.com>
2022-09-05 14:59:54 +1000

    Generated nodejs server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// var cors = require('cors');
+// app.use(cors());
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static(__dirname + '../dist/chatSystem'));
+console.log(__dirname);
+
+var http = require('http').Server(app);
+let server = http.listen(3000, function () {
+  let host = server.address().address
+  let port = server.address().port
+  console.log("Server listening on: " + host + " port: " + port)
+})
