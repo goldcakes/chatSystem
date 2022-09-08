10baa3baacd9caa175d8b4ab4828e7bf5e96e80a
Kyle <kyle.anders12@outlook.com>
2022-09-08 19:57:31 +1000

    Front end login working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { UserManagementComponent } from './user-management/user-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path:'chat', component: ChatComponent},
+  {path:'userManagement', component: UserManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const BACKURL = "http://localhost:3000"
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = "";
+
+  constructor(private router:Router, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    let  DATA = {username:this.username};
+    this.httpClient.post(BACKURL + "/api/auth", DATA, httpOptions).subscribe((res:any)=>{
+      
+      console.log("posting: " + JSON.stringify(DATA));
+      console.log("postRes: " + JSON.stringify(res));
+
+      if (res.valid) {
+        console.log("correct")
+        sessionStorage.setItem('userid', res.userid.toString());
+        sessionStorage.setItem('userlogin', res.valid.toString());
+        sessionStorage.setItem('username', res.username);
+        sessionStorage.setItem('userbirthdate', res.email);
+        sessionStorage.setItem('userage', res.role);
+
+        this.router.navigateByUrl('/account')
+      } else {
+        alert("Incorrect login")
+      }
+    })
+
   }
 
 }
