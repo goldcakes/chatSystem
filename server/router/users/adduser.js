79830c22e4c4414b50f0b2b1d34b7466e03e1a5a
Kyle <kyle.anders12@outlook.com>
2022-10-14 13:02:31 +1000

    sockets and server backend

---
+++ server/router/users/adduser.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017';
+var mongodb = require('mongodb');
+var ObjectId = mongodb.ObjectId;
+
+// Create new MongoClient
+const client = new MongoClient(url);
+
+// Function adds user to database
+module.exports = function (req, res) {
+  
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var app = express();
-
-var cors = require('cors');
+var express = require('express'); // Import express.js
+var app = express(); // use app object for express
+var http = require('http').Server(app);
+var cors = require('cors'); // Allow cross site origin requests
 app.use(cors());
+const MongoClient = require('mongodb').MongoClient; // requires MongoClient
+var ObjectID = require('mongodb').ObjectID; // requires ObjectID
+const url = 'mongodb://localhost:27017';
+// Setup for Sockets
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST']
+  }
+});
+const sockets = require('./sockets.js');
+const socketserver = require('./listen.js');
+const PORT = 3000;
+sockets.connect(io, PORT);
+socketserver.listen(http, PORT)
 
 //Enable CORS for all HTTP methods
 app.use(function (req, res, next) {
@@ -13,32 +28,36 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.json()) // Mount json middleware
 app.use(express.static(__dirname + '../dist/chatSystem'));
 console.log(__dirname);
 
-var http = require('http').Server(app);
-let server = http.listen(3000, function () {
-  let host = server.address().address
-  let port = server.address().port
-  console.log("Server listening on: " + host + " port: " + port)
-})
 
+// let server = http.listen(PORT, function () {
+//   let host = server.address().address
+//   let port = server.address().port
+//   console.log("Server listening on: " + host + " port: " + port)
+// })
+
+// login routes
 app.post('/api/auth', require('./router/postLogin'));
 app.post('/loginafter', require('./router/postLoginAfter'));
 
-app.post('/api/auth', require('./router/users/adduser'));
-app.post('/api/auth', require('./router/users/removeuser'));
-app.post('/api/auth', require('./router/users/getusers'));
+// s
+// app.post('/addUser', require('./router/users/adduser'));
+// app.post('/user/:userId', require('./router/users/removeuser'));
+// app.post('/api/auth', require('./router/users/getusers'));
 
-app.post('/loginafter', require('./router/groups/addgroup'));
-app.post('/loginafter', require('./router/groups/removegroup'));
-app.post('/loginafter', require('./router/groups/addgroupmember'));
-app.post('/loginafter', require('./router/groups/removegroupmember'));
-app.post('/loginafter', require('./router/groups/getgroups'));
+// app.post('/loginafter', require('./router/groups/addgroup'));
+// app.post('/loginafter', require('./router/groups/removegroup'));
+// app.post('/loginafter', require('./router/groups/addgroupmember'));
+// app.post('/loginafter', require('./router/groups/removegroupmember'));
+// app.post('/loginafter', require('./router/groups/getgroups'));
 
-app.post('/api/auth', require('./router/channels/addchannel'));
-app.post('/api/auth', require('./router/channels/removechannel'));
-app.post('/api/auth', require('./router/channels/addchannelmember'));
-app.post('/api/auth', require('./router/channels/removechannelmember'));
-app.post('/api/auth', require('./router/channels/getchannels'));
+// app.post('/api/auth', require('./router/channels/addchannel'));
+// app.post('/api/auth', require('./router/channels/removechannel'));
+// app.post('/api/auth', require('./router/channels/addchannelmember'));
+// app.post('/api/auth', require('./router/channels/removechannelmember'));
+// app.post('/api/auth', require('./router/channels/getchannels'));
+
+module.exports = app;
